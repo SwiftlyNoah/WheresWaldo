45dfe70c2c0621014a73f3e873a1a1455119d147
SwiftlyNoah <devnoahapps@gmail.com>
2024-03-28 12:17:56 -0400

    Initial Commit

---
+++ WheresWaldo/WheresWaldoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheresWaldoApp.swift
+//  WheresWaldo
+//
+//  Created by Noah Brauner on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WheresWaldoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
