d1a15dc82d997fe94ac9f2237394e287ec1e2f68
SwiftlyNoah <devnoahapps@gmail.com>
2024-11-10 20:24:55 -0500

    Add code to git

--- WheresWaldo/ContentView.swift
+++ WheresWaldo/ContentView.swift
@@ -8,17 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var launch = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+            
+            ZoomableScrollView {
+                Image(launch ? "waldo" : "preview")
+                    .resizable()
+                    .frame(width: launch ? nil : 40, height: launch ? nil : 40)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        .preferredColorScheme(.dark)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                launch = true
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ZoomableScrollView<Content: View>: UIViewRepresentable {
+  private var content: Content
+
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+
+  func makeUIView(context: Context) -> UIScrollView {
+    // set up the UIScrollView
+    let scrollView = UIScrollView()
+    scrollView.delegate = context.coordinator  // for viewForZooming(in:)
+    scrollView.maximumZoomScale = 20
+    scrollView.minimumZoomScale = 1
+    scrollView.bouncesZoom = true
+
+    // create a UIHostingController to hold our SwiftUI content
+    let hostedView = context.coordinator.hostingController.view!
+    hostedView.translatesAutoresizingMaskIntoConstraints = true
+    hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    hostedView.frame = scrollView.bounds
+    scrollView.addSubview(hostedView)
+
+    return scrollView
+  }
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(hostingController: UIHostingController(rootView: self.content))
+  }
+
+  func updateUIView(_ uiView: UIScrollView, context: Context) {
+    // update the hosting controller's SwiftUI content
+    context.coordinator.hostingController.rootView = self.content
+    assert(context.coordinator.hostingController.view.superview == uiView)
+  }
+
+  // MARK: - Coordinator
+
+  class Coordinator: NSObject, UIScrollViewDelegate {
+    var hostingController: UIHostingController<Content>
+
+    init(hostingController: UIHostingController<Content>) {
+      self.hostingController = hostingController
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+      return hostingController.view
+    }
+  }
 }
